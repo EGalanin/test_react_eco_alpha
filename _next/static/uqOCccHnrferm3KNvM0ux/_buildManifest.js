3ad5f193d1518e298f0f43cae15658ace7751ea1
e_galanin <galanin09234@mail.ru>
2025-11-26 09:19:33 +0300

    Updates

--- _next/static/uqOCccHnrferm3KNvM0ux/_buildManifest.js
+++ _next/static/uqOCccHnrferm3KNvM0ux/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/test_react_eco_alpha//_next/:path+",
-        "destination": "/test_react_eco_alpha/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
